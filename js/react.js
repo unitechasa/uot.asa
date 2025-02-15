81d12668a4968ec51ecbc015d5b280a4f4412bc2
Unitech Adventist Students Association <unitech.asa.info@gmail.com>
2025-02-15 23:56:47 +1000

    Delete js/react.js

--- js/react.js
+++
@@ -1,6 +0,0 @@
-//script.js
-let likeCount = 0;
-
-document.getElementByID('like-button').addEventListener('click', function() { likeCount++;
-    document.getElementByID('like-count').innerText = likeCount + 'Likes';
-    });
