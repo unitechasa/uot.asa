61db477ef9d84e08da5f308f6c7170de2b0594a1
Unitech Adventist Students Association <unitech.asa.info@gmail.com>
2025-02-17 00:24:04 +1000

    Create email.js

---
+++ js/email.js
@@ -0,0 +1,33 @@
+
+          document.addEventListener("DOMContentLoaded",()=>{
+let mail = document.getElementById("mail")
+let msg = document.getElementById("msg")
+let submit = document.getElementById("submit")
+submit.onclick = ()=>{
+    if(mail.value!=""&&msg.value!=""){
+    var templateParams = {
+  myname:"Your_Name",
+  name: mail.value,
+  message: msg.value
+};
+//emailjs.send(' your service_key','your template id', parameters)
+emailjs.send('your_service_key', 'your_template_id', templateParams)
+
+  .then(function (response) {
+    console.log('SUCCESS!', response.status, response.text);
+  }, function (error) {
+    console.log('FAILED...', error);
+  })
+  }
+  else{
+      alert("Please fill essential details")
+      //swal("Please fill essential details","","warning")
+      //(for sweetalert library...)
+      
+  }
+}
+
+    
+})
+
+            
